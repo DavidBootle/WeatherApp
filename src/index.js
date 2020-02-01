abfe226c55b885d5c0da13c0d49681097aa63057
TheWeirdSquid <47395245+TheWeirdSquid@users.noreply.github.com>
2020-01-31 21:40:09 -0500

    Readded functionality

--- src/index.js
+++ src/index.js
@@ -1,141 +1,118 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 var request = require('request');
 
-// weather info storage
-class WeatherInfo extends React.Component {
-    constructor(props) {
-        super(props);
+class Weather extends React.Component {
 
-        const defaultState = {
-            city: '',
-            weatherType: '',
-            description: '',
-            iconUrl: 'yeet',
-            currentTemp: '',
-            feelsLikeTemp: '',
-            tempMin: '',
-            tempMax: '',
-            humidity: '',
-            windSpeed: '',
-            windDeg: '',
-            infoTime: '',
-            creationTime: ''
-        }
-
-        if (props.weatherinfo !== undefined && this.state != defaultState) {
-            this.setState(props.weatherinfo);
-        } else if (props.weatherinfo !== undefined) {
-            this.state = props.weatherinfo;
-        } else {
-            this.state = defaultState;
-        }
-
-    }
-
-    render() {
-        const page = (
-            <div>
-                <h1>Current Weather Conditions In <b>{this.state.city}</b>:</h1>
-                <img src={this.state.iconUrl} alt="weather-icon"/>
-                <div className="inline">
-                    <h2 className="inline capitalize">{this.state.description}</h2>
-                </div>
-                <p>Current Temperature: {this.state.currentTemp}°F</p>
-                <p>Feels Like: {this.state.feelsLikeTemp}°F</p>
-                <p>Max: {this.state.tempMax}°F<br/>Min: {this.state.tempMin}°F</p>
-                <p>Humidity: {this.state.humidity}%</p>
-                <p>Wind Speed: {this.state.windSpeed} mph<br/>Wind Direction: {this.state.windDeg}°</p>
-                <p>Time Updated: {this.state.creationTime}</p>
-                
-            </div>
-        );
-        return page;
-    }
-}
-
-class CityNameForm extends React.Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            city: ''
+            city: props.city,
+            cityName: "--",
+            type: "--",
+            time: new Date().toString(),
+            temp: "--",
+            icon: "",
+            high: "--",
+            low: "--",
+            feelsLike: "--",
+            humidity: "--",
+            windSpeed: "--",
+            windDirection: "--"
         }
 
-        this.handleSubmitEvent = this.handleSubmitEvent.bind(this);
+        this.getData = this.getData.bind(this);
+        this.cityNameInput = this.cityNameInput.bind(this);
+
+        this.cityNameRef = React.createRef();
     }
 
-    handleSubmitEvent = (event) => {
-        event.preventDefault();
+    componentDidMount() {
+        // get inital data
+
+        // set the interval
+        setInterval(this.getData, 1000); // runs every 1 second
+    }
+
+    getData = () => {
+        // get data from weather api
         let apiKey = '45907d22480db6848331344fca6458c2';
-        let url = `http://api.openweathermap.org/data/2.5/weather?q=${this.state.city}&appid=${apiKey}&units=imperial`;
+        let city = this.cityNameRef.current.value;
+        let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
 
         request(url, function (err, response, body) {
-            changeCity(JSON.parse(body));
-        });
+            var weatherInfo = JSON.parse(body);
+
+            // if city name is invalid
+            // eslint-disable-next-line
+            if (weatherInfo.cod == 400 || weatherInfo.cod == 404) {
+                this.setState({
+                    city: this.state.city,
+                    cityName: "--",
+                    type: "--",
+                    time: new Date().toString(),
+                    temp: "--",
+                    icon: "",
+                    high: "--",
+                    low: "--",
+                    feelsLike: "--",
+                    humidity: "--",
+                    windSpeed: "--",
+                    windDirection: "--"
+                });
+            } else { // if city name is valid
+                this.setState( {
+
+                    // set properties
+                    city: weatherInfo.name, // city
+                    cityName: weatherInfo.name + ", " + weatherInfo.sys.country,
+                    type: weatherInfo.weather[0].description, // weather description e.g.(rain, snow, overcast, thunderstorm)
+                    time: new Date().toString(),
+                    temp: weatherInfo.main.temp.toString() + "°F",
+                    icon: `http://openweathermap.org/img/wn/${weatherInfo.weather[0].icon}@2x.png`,
+                    high: weatherInfo.main.temp_max.toString() + "°F",
+                    low: weatherInfo.main.temp_min.toString() + "°F",
+                    feelsLike: weatherInfo.main.feels_like.toString() + "°F",
+                    humidity: weatherInfo.main.humidity.toString() + "%",
+                    windSpeed: weatherInfo.wind.speed.toString() + " mph",
+                    windDirection: weatherInfo.wind.deg.toString() + "°"
+                } )
+            }
+        }.bind(this));
     }
 
-    handleCityChangeEvent = (event) => {
-        this.setState({city: event.target.value});
+    cityNameInput = (event) => {
+        this.setState( {city: event.target.value} );
+        setTimeout(this.getData, 100);
     }
 
     render() {
         return (
             <div>
-                <form onSubmit={this.handleSubmitEvent}>
-                    <label for="cityNameInput">City Name: </label>
-                    <input
-                        type="text"
-                        name="city"
-                        value={this.state.city}
-                        onChange={this.handleCityChangeEvent}
-                    />
-                    <input type="submit"></input>
-                </form>
+                <h1 className="capitalize">Get the Weather in <input type="text" onChange={this.cityNameInput} id="cityName" ref={this.cityNameRef}/></h1>
+                <p>City: {this.state.cityName}</p>
+                <h3>Live</h3>
+                <p className="capitalize">Weather Conditions: {this.state.type}</p>
+                <img src={this.state.icon} alt=""/>
+                <p>Temperature: {this.state.temp}</p>
+                <p>Feels Like: {this.state.feelsLike}</p>
+                <p>Humidity: {this.state.humidity}</p>
+                <p>Wind Speed: {this.state.windSpeed}</p>
+                <p>Wind Direction: {this.state.windDirection}</p>
+                <h3>Today</h3>
+                <p>High: {this.state.high}</p>
+                <p>Low: {this.state.low}</p>
+                <p className="text-muted">Server Time: {this.state.time}</p>
             </div>
         );
     }
 }
 
-// get weather info
-function getWeather(city) {
-    let apiKey = '45907d22480db6848331344fca6458c2';
-    let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+// #region Start
 
-    request(url, function (err, response, body) {
-        return JSON.parse(body);
-    });
-}
+ReactDOM.render((
+    <Weather/>
+), document.getElementById('root'));
 
-function formatDate(milliseconds) {
-    var date = new Date(milliseconds);
-    return date.getHours().toString() + ":" + date.getMinutes().toString() + ":" + date.getMilliseconds().toString();
-}
-
-function changeCity(weatherInfo) {
-    var weather = {
-        city: weatherInfo.name,
-        weatherType: weatherInfo.weather[0].main,
-        description: weatherInfo.weather[0].description,
-        iconUrl: `http://openweathermap.org/img/wn/${weatherInfo.weather[0].icon}@2x.png`,
-        currentTemp: Math.round(weatherInfo.main.temp),
-        feelsLikeTemp: Math.round(weatherInfo.main.feels_like),
-        tempMin: Math.round(weatherInfo.main.temp_min),
-        tempMax: Math.round(weatherInfo.main.temp_max),
-        humidity: Math.round(weatherInfo.main.humidity),
-        windSpeed: Math.round(weatherInfo.wind.speed),
-        windDeg: weatherInfo.wind.deg,
-        infoTime: formatDate(weatherInfo.dt),
-        creationTime: new Date().toString()
-    }
-    var renderCompile = (
-        <div>
-            <CityNameForm/>
-            <WeatherInfo weatherinfo={weather}/>
-        </div>
-    );
-    ReactDOM.render(renderCompile, document.getElementById('root'));
-}
-
-ReactDOM.render(<CityNameForm/>, document.getElementById("root"));
+// #endregion
