5940c1c3af8f8bd2fa9b4146f5bf0b74e6bfdc28
TheWeirdSquid <47395245+TheWeirdSquid@users.noreply.github.com>
2020-02-01 00:07:49 -0500

    Finished design

--- src/index.js
+++ src/index.js
@@ -10,8 +10,8 @@ class Weather extends React.Component {
         this.state = {
             city: props.city,
             cityName: "--",
-            type: "--",
-            time: new Date().toString(),
+            message: "--",
+            serverTime: getServerTime(),
             temp: "--",
             icon: "",
             high: "--",
@@ -19,7 +19,8 @@ class Weather extends React.Component {
             feelsLike: "--",
             humidity: "--",
             windSpeed: "--",
-            windDirection: "--"
+            windDirection: "--",
+            isValid: false
         }
 
         this.getData = this.getData.bind(this);
@@ -30,6 +31,8 @@ class Weather extends React.Component {
 
     componentDidMount() {
         // get inital data
+
+        this.getData();
 
         // set the interval
         setInterval(this.getData, 1000); // runs every 1 second
@@ -50,8 +53,8 @@ class Weather extends React.Component {
                 this.setState({
                     city: this.state.city,
                     cityName: "--",
-                    type: "--",
-                    time: new Date().toString(),
+                    message: "--",
+                    serverTime: getServerTime(),
                     temp: "--",
                     icon: "",
                     high: "--",
@@ -59,7 +62,8 @@ class Weather extends React.Component {
                     feelsLike: "--",
                     humidity: "--",
                     windSpeed: "--",
-                    windDirection: "--"
+                    windDirection: "--",
+                    isValid: false
                 });
             } else { // if city name is valid
                 this.setState( {
@@ -67,16 +71,17 @@ class Weather extends React.Component {
                     // set properties
                     city: weatherInfo.name, // city
                     cityName: weatherInfo.name + ", " + weatherInfo.sys.country,
-                    type: weatherInfo.weather[0].description, // weather description e.g.(rain, snow, overcast, thunderstorm)
-                    time: new Date().toString(),
-                    temp: weatherInfo.main.temp.toString() + "°F",
+                    message: getWeatherMessage(weatherInfo.weather[0].id),
+                    serverTime: getServerTime(),
+                    temp: Math.round(weatherInfo.main.temp).toString() + "°F",
                     icon: `http://openweathermap.org/img/wn/${weatherInfo.weather[0].icon}@2x.png`,
-                    high: weatherInfo.main.temp_max.toString() + "°F",
-                    low: weatherInfo.main.temp_min.toString() + "°F",
-                    feelsLike: weatherInfo.main.feels_like.toString() + "°F",
-                    humidity: weatherInfo.main.humidity.toString() + "%",
-                    windSpeed: weatherInfo.wind.speed.toString() + " mph",
-                    windDirection: weatherInfo.wind.deg.toString() + "°"
+                    high: Math.round(weatherInfo.main.temp_max).toString() + "°F",
+                    low: Math.round(weatherInfo.main.temp_min).toString() + "°F",
+                    feelsLike: Math.round(weatherInfo.main.feels_like).toString() + "°F",
+                    humidity: Math.round(weatherInfo.main.humidity).toString() + "%",
+                    windSpeed: Math.round(weatherInfo.wind.speed).toString() + " mph",
+                    windDirection: Math.round(weatherInfo.wind.deg) + "°",
+                    isValid: true
                 } )
             }
         }.bind(this));
@@ -88,25 +93,175 @@ class Weather extends React.Component {
     }
 
     render() {
-        return (
-            <div>
-                <h1 className="capitalize">Get the Weather in <input type="text" onChange={this.cityNameInput} id="cityName" ref={this.cityNameRef}/></h1>
-                <p>City: {this.state.cityName}</p>
-                <h3>Live</h3>
-                <p className="capitalize">Weather Conditions: {this.state.type}</p>
-                <img src={this.state.icon} alt=""/>
-                <p>Temperature: {this.state.temp}</p>
-                <p>Feels Like: {this.state.feelsLike}</p>
-                <p>Humidity: {this.state.humidity}</p>
-                <p>Wind Speed: {this.state.windSpeed}</p>
-                <p>Wind Direction: {this.state.windDirection}</p>
-                <h3>Today</h3>
-                <p>High: {this.state.high}</p>
-                <p>Low: {this.state.low}</p>
-                <p className="text-muted">Server Time: {this.state.time}</p>
-            </div>
-        );
+        if (this.state.isValid) {
+            return (
+                <div>
+                    <div class="container pb-4">
+                        <h1 class="display-4 col-xl-12">Get the Weather in <input type="text" onChange={this.cityNameInput} ref={this.cityNameRef}/></h1>
+                    </div>
+                    <div class="container">
+                        <img src={this.state.icon} width="100" height="100" alt=""/>
+                        <h2 style={{display: "inline"}}>In {this.state.cityName}, {this.state.message}</h2>
+                    </div>
+                    <div class="container">
+                        <div class="row">
+                            <div class="card col-md-4">
+                                <div class="card-body">
+                                    <h5 class="card-title">Temperature</h5>
+                                    <p class="card-text">Right now in {this.state.city} it's <strong>{this.state.temp}</strong>, with a high of {this.state.high} and a low of {this.state.low}. Outside, it feels like {this.state.feelsLike}.&nbsp; &nbsp; &nbsp; &nbsp;&nbsp;</p>
+                                </div>
+                            </div>
+                            <div class="card col-md-4">
+                                <div class="card-body">
+                                        <h5 class="card-title">Wind</h5>
+                                        <p class="card-text">The wind is currently {this.state.windSpeed} at {this.state.windDirection}.&nbsp; &nbsp; &nbsp;&nbsp;</p>
+                                </div>
+                            </div>
+                            <div class="card col-md-4">
+                                <div class="card-body">
+                                    <h5 class="card-title">Humidity<strong>&nbsp;</strong></h5>
+                                    <p class="card-text">&nbsp;The humidity is currently <strong>{this.state.humidity}.&nbsp;</strong>&nbsp;</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="container small mt-2">
+                        <p className="text-muted">{this.state.serverTime}</p>
+                    </div>
+                </div>
+            );
+        } else {
+            return (
+                <div>
+                    <div class="container pb-4">
+                        <h1 class="display-4 col-xl-12">Get the Weather in <input type="text" onChange={this.cityNameInput} ref={this.cityNameRef}/></h1>
+                    </div>
+                    <div class="container small">
+                        <p className="text-muted">{this.state.serverTime}</p>
+                    </div>
+                </div>
+            );
+        }
+            
     }
+}
+
+function getWeatherMessage(weatherCode) {
+    // eslint-disable-next-line
+    switch (weatherCode) {
+        case 200: return "it's storming. Expect light rain and lightning.";
+        case 201: return "it's storming. Expect rain and lightning";
+        case 202: return "it's storming. Expect heavy rain and lightning.";
+        case 210: return "it's storming. Expect light storms.";
+        case 211: return "it's storming.";
+        case 212: return "it's storming. Expect heavy storms.";
+        case 221: return "it's storming. Expect scattered storms.";
+        case 230: return "it's storming. Expect a light drizzle.";
+        case 231: return "it's storming. Expect a medium drizzle.";
+        case 232: return "it's storming. Expect a heavy drizzle.";
+
+        case 300: return "it's drizzling. Expect a light drizzle.";
+        case 301: return "it's drizzling.";
+        case 302: return "it's drizzling. Expect a heavy drizzle.";
+        case 310: return "it's drizzling. Expect a light drizzle mixed with rain.";
+        case 311: return "it's drizzling. Expect a medium drizzle mixed with rain.";
+        case 312: return "it's drizzling. Expect a heavy drizzle mixed with rain.";
+        case 313: return "it's drizzling. Expect drizzling mixed with scattered showers.";
+        case 314: return "it's drizzling. Expect drizzling mixed with scattered heavy showers.";
+        case 321: return "the weather service says \"shower drizzle\". So whatever that is, I guess.";
+        
+        case 500: return "it's raining. Expect light rain.";
+        case 501: return "it's raining. Expect moderate rain.";
+        case 502: return "it's raining. Expect heavy rain.";
+        case 503: return "it's raining. Expect very heavy rain.";
+        case 504: return "it's raining. Expect extreme rain.";
+        case 511: return "it's raining. Expect freezing rain.";
+        case 520: return "it's raining. Expect light showers.";
+        case 521: return "it's raining. Expect showers.";
+        case 522: return "it's raining. Expect heavy showers.";
+        case 531: return "it's raining. Expect scattered showers.";
+
+        case 600: return "it's snowing. Expect light snow.";
+        case 601: return "it's snowing."
+        case 602: return "it's snowing. Expect heavy snow.";
+        case 611: return "it's sleeting.";
+        case 612: return "it's sleeting. Expect light sleet showers.";
+        case 613: return "it's sleeting. Expect sleet showers.";
+        case 615: return "it's snowing. Expect mixed light rain and snow.";
+        case 616: return "it's snowing. Expect mixed rain and snow.";
+        case 620: return "it's snowing. Expect light snow showers.";
+        case 621: return "it's snowing. Expect snow showers.";
+        case 622: return "it's snowing. Expect heavy snow showers.";
+
+        case 701: return "it's misty.";
+        case 711: return "it's smoky.";
+        case 721: return "it's hazy.";
+        case 731: return "there are dust devils.";
+        case 741: return "it's foggy.";
+        case 751: return "it's sandy.";
+        case 761: return "it's dusty.";
+        case 762: return "there is volcanic ash.";
+        case 771: return "there are squalls.";
+        case 781: return "there are tornados.";
+
+        case 800: return "there are clear skies.";
+
+        case 801: return "there are a few clouds.";
+        case 802: return "there are scattered clouds.";
+        case 803: return "it's cloudy.";
+        case 804: return "it's overcast.";
+    }
+}
+
+function getDay(dayNumber) {
+    // eslint-disable-next-line
+    switch (dayNumber) {
+        case 0: return "Sunday";
+        case 1: return "Monday";
+        case 2: return "Tuesday";
+        case 3: return "Wednesday";
+        case 4: return "Thursday";
+        case 5: return "Friday";
+        case 6: return "Saturday";
+    }
+}
+
+function getMonth(month) {
+    // eslint-disable-next-line
+    switch (month) {
+        case 0: return "January";
+        case 1: return "February";
+        case 2: return "March";
+        case 3: return "April";
+        case 4: return "May";
+        case 5: return "June";
+        case 6: return "July";
+        case 7: return "August";
+        case 8: return "September";
+        case 9: return "October";
+        case 10: return "November";
+        case 11: return "December";
+    }
+}
+
+function getServerTime() {
+    var serverTime = new Date();
+    var timeInfo = {};
+    timeInfo.day = getDay(serverTime.getDay());
+    timeInfo.date = serverTime.getDate();
+    timeInfo.year = serverTime.getFullYear();
+    if (serverTime.getHours() < 12) {
+        timeInfo.hour = serverTime.getHours().toString().padStart(2, "0");
+        timeInfo.timeOfDay = "AM";
+    } else {
+        timeInfo.hour = (serverTime.getHours() - 12).toString().padStart(2, "0");
+        timeInfo.timeOfDay = "PM";
+    }
+    timeInfo.minute = serverTime.getMinutes().toString().padStart(2, "0");
+    timeInfo.second = serverTime.getSeconds().toString().padStart(2, "0");
+    timeInfo.month = getMonth(serverTime.getMonth());
+
+    return `${timeInfo.day}, ${timeInfo.month} ${timeInfo.date} ${timeInfo.year}, ${timeInfo.hour}:${timeInfo.minute}:${timeInfo.second} ${timeInfo.timeOfDay}`;
 }
 
 // #region Start
